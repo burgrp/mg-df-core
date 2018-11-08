ced887c5fdc16fcf3728975c69aca9aa7faf2783
Pavel Burgr <pb@drake.cz>
2018-11-09 00:10:21 +0100

    WIP

--- fs/api_df_reg.js
+++ fs/api_df_reg.js
@@ -1,44 +1,38 @@
 /* global Cfg, MQTT */
 
 load('api_mqtt.js');
-load("api_config.js");
 
 let Register = {
 	add: function (name, register) {
 
-		let mqttId = Cfg.get("mqtt.client_id") || Cfg.get("device.id");
-
 		let observer = {
 			name: name,
-			mqttId: mqttId,
 			callback: function (value) {
-				MQTT.pub(this.mqttId + "/register/" + this.name + "/is", JSON.stringify(value));
+				MQTT.pub("register/" + this.name + "/is", JSON.stringify(value));
 			}
 		};
 
 		register.observer = observer;
 
-		MQTT.sub(mqttId + "/register/" + name + "/#", function (conn, topic, msg, ctx) {
+		MQTT.sub("register/" + name + "/get", function (conn, topic, msg, ctx) {
+			ctx.register.get();
+		}, {
+				register: register
+			});
 
-			let lastThree = topic.slice(topic.length - 3, topic.length);
+		MQTT.sub("register/" + name + "/set", function (conn, topic, msg, ctx) {
 
-			if (lastThree === "set") {
-				let value = JSON.parse(msg);
-				print(ctx.name, "=>", value);
-				if (ctx.register.set) {
-					ctx.register.set(value);
-				}
-				ctx.register.get();
+			let value = JSON.parse(msg);
+			print(ctx.name, "=>", value);
+			if (ctx.register.set) {
+				ctx.register.set(value);
 			}
-
-			if (lastThree === "get") {
-				ctx.register.get();
-			}
+			ctx.register.get();
 
 		}, {
-			name: name,
-			register: register
-		});
+				name: name,
+				register: register
+			});
 
 		return register;
 	}
